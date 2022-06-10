5ae198d5442bff72d66907debdd793f9c0bd0134
Rakhat <95361982+latest-update@users.noreply.github.com>
2022-06-11 03:48:37 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require ( "express" );
+const db = require ( "./db.js" );
+const isEmpty = require('lodash.isempty');
+
+const app = express ();
+const jsonParser = express.json();
+const database = new db();
+
+app.post("/save", jsonParser, (request, response) => {
+    if ( isEmpty(request.body) )
+        response.status(400).json(
+            { message: "Post request has empty body" }
+        );
+    const sql = "INSERT INTO apps (id, data) VALUES (NULL, ?)";
+    const data = [JSON.stringify(request.body)];
+
+    database.connection.query(sql, data)
+        .then(res => {
+            response.status(201).json(
+                { message: "Successfully", id: res[0].insertId }
+            );
+        })
+        .catch(err => {
+            response.status(500).json(
+                { message: "Something goes wrong" }
+            );
+        });
+});
+
+app.get("/:id", (request, response) => {
+
+    database.connection.query(`SELECT * FROM apps WHERE id = ${request.params.id}`)
+        .then(res => {
+            let data = JSON.parse( res[0][0].data );
+            response.json(data);
+        })
+        .catch(err => {
+            console.log(err)
+            response.status(404).json(
+                { message: "Couldn't found such app" }
+            );
+        });
+
+});
+
+app.listen(3000);
---
+++ db.js
@@ -0,0 +1,27 @@
+const mysql = require ( "mysql2" );
+require( "dotenv" ).config();
+
+class db {
+    constructor () {
+        this.connection = mysql.createConnection({
+            host: process.env.HOST,
+            user: process.env.USER,
+            database: process.env.DATABASE,
+            password: process.env.PASSWORD
+        }).promise();
+        const sql = `create table if not exists apps (
+              id int primary key auto_increment,
+              data longtext COLLATE utf8mb4_general_ci
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci`
+
+        this.connection.query(sql)
+            .then(res => {
+                console.log("Table has created");
+            })
+            .catch(err => {
+                console.log("Error")
+            });
+    }
+}
+
+module.exports = db;
